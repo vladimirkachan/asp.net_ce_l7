2a5619bf2a7a443bb43bd248bbd61a95922994b9
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-19 08:21:58 +0300

    Add project files.

---
+++ 01-DependencyInjection/Services/EmailValidatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01_DependencyInjection.Services
+{
+    public class EmailValidatorService : IEmailValidator
+    {
+        public bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, "\\w + ([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
+        }
+    }
+}
---
+++ 01-DependencyInjection/Services/IEmailValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_DependencyInjection.Services
+{
+    public interface IEmailValidator
+    {
+        bool IsValidEmail(string email);
+    }
+}
